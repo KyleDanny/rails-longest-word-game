047c0b094bb8ca51b0e05ee17a836bb347c0b7ec
Kyle Danny <codingkyle92@gmail.com>
2021-04-29 19:51:29 +0200

    completed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,14 +6,15 @@ class GamesController < ApplicationController
 
   def new
     # generate random grid of 10 letters
-    $letters = generate_grid
+    @grid = generate_grid
     @current_score = current_score
   end
 
   def score
     @word = params[:word]
-
-    if included?(@word.upcase, $letters)
+    @letters = params[:random_grid]
+    raise
+    if included?(@word.upcase, @letters)
       if english_word?(@word)
         @results = "Congratulations #{@word.upcase} is a valid English word!"
         increment_score_count(@word)
@@ -23,7 +24,7 @@ class GamesController < ApplicationController
         session[:score_count] = 0
       end
     else
-      separate_letters = $letters.join(', ')
+      separate_letters = @letters.join(', ')
       @results = "Sorry but #{@word.upcase} can't be built out of #{separate_letters}"
       @final_score = "Final Score: #{current_score}"
       session[:score_count] = 0
@@ -53,8 +54,8 @@ class GamesController < ApplicationController
   def increment_score_count(word)
     # session[:score_count] += word.length * word.length
     word.upcase.each_char do |letter|
-    session[:score_count] += SCORING[letter.to_sym]
-  end
+      session[:score_count] += SCORING[letter.to_sym]
+    end
   end
 
   def current_score
@@ -63,12 +64,14 @@ class GamesController < ApplicationController
 end
 
 SCORING = {
-    A: 1,   B: 3,   C: 3,   D: 2,   E: 1,   F: 4,   G: 2,   H: 4,   I: 1,
-    J: 8,   K: 5,   L: 1,   M: 3,   N: 1,   O: 1,   P: 3,   Q: 10,  R: 1,
-    S: 1,   T: 1,   U: 1,   V: 4,   W: 4,   X: 8,   Y: 4,   Z: 10
-  }
+  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1,
+  J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1,
+  S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10
+}
+
 # => Questions
 # global variable $letters, not @letters?
 # Why POST request? Why not use GET?
 # hidden_field_tag - not understanding why we need this?
 # reason for authenticy token
+# css - white?
